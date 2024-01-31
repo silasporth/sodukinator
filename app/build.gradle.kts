7795922f4b61efc79e789b1b7568df826a672e6e
Silas Porth <silas@porth-odernheim.de>
2024-01-31 13:33:20 +0100

    Minor dependency changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,6 @@ android {
         jvmTarget = "17"
     }
     buildFeatures {
-        compose = true
         viewBinding = true
     }
     composeOptions {
@@ -54,8 +53,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation(project(":OpenCV"))
     testImplementation("junit:junit:4.13.2")
